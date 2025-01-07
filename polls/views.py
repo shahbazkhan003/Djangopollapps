5cb0c02b54c9e4b3f7c6198d664ec33e6e3b9d2f
shahbaz khan <sk9200919@gmail.com>
2025-01-07 17:17:04 +0500

    add api

--- polls/views.py
+++ polls/views.py
@@ -4,41 +4,42 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import QuestionSerializer
 
 
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
 
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "polls/detail.html"
+class IndexView(APIView):
+    def get(self, request):
+        latest_questions = Question.objects.order_by('pub_date')[-5]
+        serializer = QuestionSerializer(latest_questions, many=True)
+        return Response(serializer.data)
 
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "polls/results.html"
+class DetailView(APIView):
+    def get(request,self,pk):
+        question = get_object_or_404(Question,pk=pk)
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
 
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        return render(
-            request,
-            "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
-    else:
-        selected_choice.votes = F("votes") + 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+class ResultsView(APIView):
+    def get(request,self,pk):
+        question = get_object_or_404(Question, pk=pk)
+        serializer = QuestionSerializer(question)
+        return Response(serializer.data)
+
+class vote(APIView):
+    def post(self, request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.data["choice"])
+        except (KeyError, Choice.DoesNotExist):
+            return Response({"error": "You didn't select a choice."}, status=400)
+        else:
+            selected_choice.votes = F("votes") + 1
+            selected_choice.save()
+            serializer = QuestionSerializer(question)
+            return Response(serializer.data)
