a710b2fb205f8c9f2a8a3fbb197a736ed6277739
shahbaz khan <sk9200919@gmail.com>
2025-01-10 22:43:07 +0500

    add rest registraction

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Ecommerence API",
+      title="Polls API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
@@ -34,9 +34,13 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+api_urlpatterns = [
+    path('accounts/', include('rest_registration.api.urls')),
+]
 
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path("polls/", include("polls.urls")),
+    path('api/v1/', include(api_urlpatterns)),
 ]
