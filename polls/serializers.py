6145ebaf2d0770f5806670c207d2e5048fc2418c
shahbaz khan <sk9200919@gmail.com>
2025-01-13 14:41:07 +0500

    fix more bugs

--- polls/serializers.py
+++ polls/serializers.py
@@ -7,6 +7,3 @@ class QuestionSerializer(serializers.ModelSerializer):
         fields = ['id', 'question_text', 'pub_date']
         
         
-class Loginserializer(serializers.Serializer):
-    username = serializers.CharField(max_length=100)
-    password = serializers.CharField(max_length=100)        
